92adabc961c673e21c072ee60a0ad6f09633b7f1
CaloMorell <oscarmorell07@yahoo.com>
2023-11-01 10:18:50 -0400

    Defines the User class

--- models/user.py
+++ models/user.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""Defines the User class"""
+from models.base_model import BaseModel
+
+class Usuario(BaseModel):
+    """Atribute:
+    
+    email (str): The email of the user.
+    password (str): The password of the user.
+    Frist_name (str): The frist name of the user.
+    last_name (str): The last name of the user. """
+    
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
