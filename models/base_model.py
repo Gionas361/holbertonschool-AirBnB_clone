e5b30267cfaeac4a267ad74f3c64c2f42196b942
CaloMorell <oscarmorell07@yahoo.com>
2023-11-01 10:09:56 -0400

    update

--- models/base_model.py
+++ models/base_model.py
@@ -9,7 +9,7 @@ class BaseModel():
         """Initialize variables"""
         
         tform = "%Y-%m-%dT%H:%M:%S.%f"
-        self.id = str(uuid.uidd4())
+        self.id = str(uuid.uuid4())
         self.created_at = None
         self.updated_at = None
 
