cc7d65459ab48244661c567a47f9783e93a96883
Gionas361 <giovannicarmona57@gmail.com>
2023-11-02 11:38:16 -0400

    code

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,51 @@
 #!/usr/bin/python3
-"""This is documentation"""
-import uuid
+"""Defines the BaseModel class."""
+import models
+from uuid import uuid4
+from datetime import datetime
 
 
-class BaseModel():
+class BaseModel:
+    """Represents the BaseModel of the HBnB project."""
 
-    def __init__(self):
-        """Initialize variables"""
+    def __init__(self, *args, **kwargs):
+        """Initialize a new BaseModel.
 
-        self.id = str(uuid.uidd4())
-        self.created_at = None
-        self.updated_at = None
-
-    def __str__(self):
-        """__str__: should print: [<class name>]
-        (<self.id>) <self.__dict__>"""
-
-        return
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def save(self):
-        """save(self): updates the public
-        instance attribute updated_at with
-        the current datetime"""
-
-        return
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
-        """to_dict(self): returns a dictionary containing
-        all keys/values of __dict__ of the instance:
+        """Return the dictionary of the BaseModel instance.
 
-        by using self.__dict__, only instance attributes set will be returned
-        a key __class__ must be added to this dictionary with the class name of the object
-        created_at and updated_at must be converted to string object in ISO format:
-            format: %Y-%m-%dT%H:%M:%S.%f (ex: 2017-06-14T22:31:03.285259)
-            you can use isoformat() of datetime object
-        This method will be the first piece of the serialization/deserialization process: create a dictionary representation with “simple object type” of our BaseModel"""
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
