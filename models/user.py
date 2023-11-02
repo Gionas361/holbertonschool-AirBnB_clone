369fa0d990ecfa03873805b53f256511096a95fd
CaloMorell <oscarmorell07@yahoo.com>
2023-11-02 00:57:20 -0400

    The User class

--- models/user.py
+++ models/user.py
@@ -3,12 +3,7 @@
 from models.base_model import BaseModel
 
 class Usuario(BaseModel):
-    """Atribute:
-    
-    email (str): The email of the user.
-    password (str): The password of the user.
-    Frist_name (str): The frist name of the user.
-    last_name (str): The last name of the user. """
+    """the User class"""
     
     email = ""
     password = ""
